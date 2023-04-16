38910e95f0d6b0a6262259ac82cf7fdf91275dea
bibekshhh <bibekrock2019@gmail.com>
2023-04-16 18:46:52 +0545

    updated API_URL for deployment

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,7 +1,6 @@
-const API_URL = "http://localhost:9000";
+const API_URL = "https://meroshare-pro.herokuapp.com";
 
 export default API_URL;
 
 // for production backend
-
 // https://meroshare-pro.herokuapp.com
